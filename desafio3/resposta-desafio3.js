921f9bcf0daec1c998f0f711fdc3cad8b8bd0f8c
Leandro-Rosa <orosa.leandro@gmail.com>
2022-07-23 19:25:40 -0300

    Desafio3 - TypeScript

---
+++ desafio3/resposta-desafio3.js
@@ -0,0 +1,34 @@
+"use strict";
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+// export {} // Para não reclamar de variáveis duplicadas
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+if (campoSaldo) {
+    campoSaldo.innerHTML = '0';
+}
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        let valor_saldo = Number(campoSaldo.innerHTML);
+        valor_saldo += soma;
+        campoSaldo.innerHTML = valor_saldo.toString();
+    }
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        campoSaldo.innerHTML = '';
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        if (soma) {
+            somarAoSaldo(Number(soma.value));
+        }
+    });
+}
+if (botaoLimpar) {
+    botaoLimpar.addEventListener('click', function () {
+        limparSaldo();
+    });
+}
